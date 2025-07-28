aacc2761a2afc17f44911aef4fe2d9ab31fa7874
kchaeeun <oceanball38@naver.com>
2025-07-29 02:52:30 +0900

    ãrefactor : ãê 근처 가게 찾는 Native 쿼리 추가

--- gusto/src/main/java/com/umc/gusto/domain/store/repository/StoreRepository.java
+++ gusto/src/main/java/com/umc/gusto/domain/store/repository/StoreRepository.java
@@ -9,6 +9,12 @@ import java.util.List;
 public interface StoreRepository extends JpaRepository<Store, Long> {
     @Query("SELECT s FROM Store s WHERE s.town.townCode = :townCode AND s.storeId IN :storeIds")
     List<Store> findByTownCodeAndStoreIds(String townCode, List<Long> storeIds);
+    // 네이티브 쿼리
+    @Query(value = """
+        SELECT * FROM sotre
+        WHERE ST_Distance_Sphere(location, POINT(:longitude, :latitude)) <= :radius
+    """, nativeQuery = true)
+    List<Store> findStoresWithinRadius(Double latitude, Double longitude, int radius);
 
     List<Store> findTop5ByStoreNameContains(String keyword);
 }
